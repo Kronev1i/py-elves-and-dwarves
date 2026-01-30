a9d2c27d80378c5a0799cc9e6f10a973bbffd374
unknown <kgoncharenko83@gmail.com>
2026-01-30 22:29:35 +0100

    Fix abstract method

--- app/players/dwarves/dwarf.py
+++ app/players/dwarves/dwarf.py
@@ -1,20 +1,10 @@
 from app.players.player import Player
-from abc import ABC, abstractmethod
 
 
-class Dwarf(Player, ABC):
-    def __init__(
-            self,
-            nickname: str,
-            favourite_dish: str
-    ) -> None:
+class Dwarf(Player):
+    def __init__(self, nickname: str, favourite_dish: str):
         super().__init__(nickname)
         self._favourite_dish = favourite_dish
 
-    @abstractmethod
     def eat_favourite_dish(self) -> None:
-        print(f"{self.nickname}"
-              f" is eating {self._favourite_dish}")
-
-    def declared(self) -> None:
-        pass
+        print(f"{self.nickname} is eating {self._favourite_dish}")
--- app/players/elves/elf.py
+++ app/players/elves/elf.py
@@ -1,20 +1,10 @@
 from app.players.player import Player
-from abc import ABC, abstractmethod
 
 
-class Elf(Player, ABC):
-    def __init__(
-            self,
-            nickname: str,
-            musical_instrument: str
-    ) -> None:
+class Elf(Player):
+    def __init__(self, nickname: str, musical_instrument: str):
         super().__init__(nickname)
         self._musical_instrument = musical_instrument
 
-    @abstractmethod
     def play_elf_song(self) -> None:
-        print(f"{self.nickname} is playing a song"
-              f" on the {self._musical_instrument}")
-
-    def declared(self) -> None:
-        pass
+        print(f"{self.nickname} is playing a song on the {self._musical_instrument}")
